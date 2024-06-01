6410d7e2aa23e6b806c30b610adeb9b5ac2285a1
Denis Talaluev <denis.tal@mail.ru>
2024-06-02 03:14:22 +1000

    feat(workspace): Добавить создание папки при первом запуске

--- models/DuckingModel/download.py
+++ models/DuckingModel/download.py
@@ -1,9 +1,15 @@
+import os
 import gdown
 
 # URL файла или его идентификатор
 url = "https://drive.google.com/uc?id=1z08IQnnQE8LcQKmrtfycvfvNVqHyjmy7"
 # Путь, куда будет сохранён файл
-output = "./models/DuckingModel/weights/model.h5"
+output_dir = "./models/DuckingModel/weights"
+output_file = os.path.join(output_dir, "model.h5")
+
+# Проверяем, существует ли директория, если нет - создаём
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
 # Скачивание файла
-gdown.download(url, output, quiet=False)
+gdown.download(url, output_file, quiet=False)
